9303837a117ab138cdd4e8958f8369e5b421a00c
Sirje S <sirtsu24@hotmail.com>
2019-09-20 17:59:03 +0300

    Ternary Operator

--- liigaasta/Program.cs
+++ liigaasta/Program.cs
@@ -6,23 +6,38 @@ namespace liigaasta
     {
         static void Main(string[] args)
         {
-            
-            int yearOfBirth;
-            //liigaasta - aastaarv jagub 4ga//
-            int y = 4;
-            
-            Console.WriteLine("Please enter your year of birth: ");
-            yearOfBirth = Int32.Parse(Console.ReadLine());
-            int x = yearOfBirth;
-            Console.WriteLine(yearOfBirth % y);
+
+            /*int yearOfBirth;
+             //liigaasta - aastaarv jagub 4ga//
 
 
-            if (x % y == 0)
-            {
-                Console.WriteLine("Liigaasta");
-            }
-            else
-                Console.WriteLine("Ei ole liigaasta");
+             Console.WriteLine("Please enter your year of birth: ");
+             yearOfBirth = Int32.Parse(Console.ReadLine());
+
+
+              if (yearOfBirth % 4 == 0)
+              {
+                  Console.WriteLine("Liigaasta");
+              }
+              else
+                  Console.WriteLine("Ei ole liigaasta");*/
+
+
+            //Ternary operator - töötab vaid siis kui on kaks võimalust//
+            /*string answer = (yearOfBirth % 4 == 0) ? "Liigaasta." : "Ei ole liigaasta";
+            Console.WriteLine(answer);*/
+
+            
+            
+            
+            Console.WriteLine("Choose your poison, whisky or wine");
+            string drink = Console.ReadLine();
+            
+
+            string answer = drink == "wine" ? "Here's your wine" : "Here's your whisky";
+            Console.WriteLine(answer);
+
+
             Console.ReadLine();
 
 
