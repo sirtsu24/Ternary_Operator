46a712b43a364e4783092db82f8972b2ee22abde
Sirje S <sirtsu24@hotmail.com>
2019-09-20 17:18:46 +0300

    Add project files.

---
+++ liigaasta/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace liigaasta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int yearOfBirth;
+            //liigaasta - aastaarv jagub 4ga//
+            int y = 4;
+            
+            Console.WriteLine("Please enter your year of birth: ");
+            yearOfBirth = Int32.Parse(Console.ReadLine());
+            int x = yearOfBirth;
+            Console.WriteLine(yearOfBirth % y);
+
+
+            if (x % y == 0)
+            {
+                Console.WriteLine("Liigaasta");
+            }
+            else
+                Console.WriteLine("Ei ole liigaasta");
+            Console.ReadLine();
+
+
+        }
+    }
+}
